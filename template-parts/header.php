facd4990f1938a53e5e83974bb9eb3f472944594
James Hill <jhill7177@gmail.com>
2020-07-02 03:08:02 +1000

    logo url bug fix 2

--- template-parts/header.php
+++ template-parts/header.php
@@ -13,7 +13,7 @@
         $custom_logo_id = get_theme_mod( 'custom_logo' );
         $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
         if(has_custom_logo()){
-          echo '<a href="' . get_bloginfo('url') . '"><img src="' . esc_url( $logo&#91;0&#93; ) . '" alt="' . get_bloginfo( 'name' ) . '"></a>';
+          echo '<a href="' . get_bloginfo('url') . '"><img src="' . esc_url($logo) . '" alt="' . get_bloginfo( 'name' ) . '"></a>';
         } else {
           echo '<h1><a href="' . get_bloginfo('url') . '">' . get_bloginfo( 'name' ) .'</a></h1>';
         }
