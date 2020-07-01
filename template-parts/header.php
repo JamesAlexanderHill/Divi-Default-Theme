2f2b1aec4e0fcdc93c4e958b8c12261d469aefcf
James Hill <jhill7177@gmail.com>
2020-07-02 03:00:22 +1000

    add custom logo to site with fall back

--- template-parts/header.php
+++ template-parts/header.php
@@ -9,7 +9,15 @@
     <?php wp_body_open(); ?>
     <header>
       <div class="page-width">
-        <h1><a href="<?php echo get_bloginfo('url'); ?>"><?php echo get_bloginfo('name'); ?></a></h1>
+        <?php
+        $custom_logo_id = get_theme_mod( 'custom_logo' );
+        $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+        if(has_custom_logo()){
+          echo '<a href="' . get_bloginfo('url') . '"><img src="' . $logo . '" alt="' . get_bloginfo( 'name' ) . '"></a>';
+        } else {
+          echo '<h1><a href="' . get_bloginfo('url') . '>' . get_bloginfo( 'name' ) .'</a></h1>';
+        }
+        ?>
         <?php
         wp_nav_menu(
           array(
