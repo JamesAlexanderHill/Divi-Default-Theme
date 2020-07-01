e50f04459d1eec888d52e3359d0f8b709c0e8bba
James Hill <jhill7177@gmail.com>
2020-07-02 02:48:02 +1000

    add theme support (title, logo img) removed margin on body

--- functions.php
+++ functions.php
@@ -6,7 +6,19 @@ function add_theme_scripts() {
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
 //Theme Support
-add_theme_support( 'title-tag' );
+
+function add_theme_support_funcs() {
+ $logo_param = array(
+   'height'      => 100,
+   'width'       => 400,
+   'flex-height' => true,
+   'flex-width'  => true,
+   'header-text' => array( 'site-title', 'site-description' ),
+ );
+ add_theme_support( 'custom-logo', $logo_param );
+ add_theme_support( 'title-tag' );
+}
+add_action( 'after_setup_theme', 'add_theme_support_funcs' );
 
 //Menus
 function register_menus() {
